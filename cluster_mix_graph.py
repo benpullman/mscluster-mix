7beb562ca3888cf42fbbdd518a4878ffd25d7c21
Ben Pullman <bp2@icloud.com>
2016-03-02 11:18:00 -0800

    initialized clusters

--- cluster_mix_graph.py
+++ cluster_mix_graph.py
@@ -1,11 +1,42 @@
 import networkx as nx
 import sys
 from collections import Counter
+import pickle
 
 class Cluster:
-    def __init__(self,a_edges,b_edges):
-        self.a_edges = a_edges
-        self.b_edges = b_edges
+    def __init__(self,number,a_graph,b_graph):
+        self.number = number
+        self.a_graph = a_graph
+        self.b_graph = b_graph
+    def nodes(self):
+        return self.a_graph.nodes()
+    def a_edges(self):
+        return self.a_graph.edges()
+    def b_edges(self):
+        return self.b_graph.edges()
+    def split_at(self,e1,e2):
+        self.a_graph.remove_edge(e1,e2)
+        self.a_graph.remove_edge(e2,e1)
+        new_a_graphs = nx.connected_component_subgraphs(a_graph)
+        new_clusters = []
+        for a in new_a_graphs:
+            b = nx.Graph()
+            for n1 in a:
+                for n2 in a:
+                    if n1 != n2:
+                        new_b_graph.add_edge(n1,n2,weight=b_graph[n1][n2]['weight'])
+            new_clusters.append(Cluster(self.number + ".a",a,b))
+        return new_clusters
+
+def pickle_clusters(clusters,filename = "clusters.p"):
+    with open(filename, "wb") as f:
+        pickle.dump(clusters,f)
+
+def load_clusters(filename = "clusters.p"):
+    clusters = []
+    with open(filename, "rb") as f:
+        clusters = pickle.load(f)
+    return clusters
 
 def generate_identification(filename):
     identifcation = {}
@@ -77,8 +108,14 @@ def all_id(cc,ids):
     c_ids = cluster_ids(cc,ids)
     return not 'PEPTIDE' in c_ids
 
-# def link_edges(a_clusters,b_clusters):
-#     clusters = [];
+def link_edges(a_clusters,b_clusters):
+    clusters = []
+    for cluster_id in a_clusters:
+        try:
+            clusters.append(Cluster(cluster_id,a_clusters[cluster_id],b_clusters[cluster_id]))
+        except:
+            pass
+    return clusters
 
 
 def seperate_mixtures(graph):
@@ -97,8 +134,11 @@ def seperate_mixtures(graph):
         elif len(clust)==2 and 'PEPTIDE' in clust:
             sp += 1
         elif mix(cluster,ids):
-            key = [n for n in sorted(cluster.nodes())][0]
-            mix_graphs[key] = cluster
+            try:
+                key = cluster_number[cluster.nodes()[0]]
+                mix_graphs[key] = cluster
+            except:
+                pass
             m += 1
             # if avg_cc_weight(cluster) < .7:
             #     for edge in cluster.edges():
@@ -112,22 +152,16 @@ def seperate_mixtures(graph):
     # print("Error: " + str(e))
     return mix_graphs
 
-def split_cluster(a_graph,b_graph,e1,e2):
-    new_b_graphs=nx.Graph()
-    a_graph.remove_edge(e1,e2)
-    a_graph.remove_edge(e2,e1)
-    new_a_graphs = nx.connected_component_subgraphs(a_graph)
-    for a in new_a_graphs:
-        for n1 in a:
-            for n2 in a:
-                if n1 != n2:
-                    new_b_graphs.add_edge(n1,n2,weight=b_graph[n1][n2]['weight'])
-    return nx.connected_component_subgraphs(new_b_graphs), nx.connected_component_subgraphs(new_a_graphs)
-
-
-a_graph = generate_a_edges(sys.argv[1])
-b_graph = generate_b_edges(sys.argv[2])
-ids, cluster_number = generate_identification(sys.argv[3])
+clusters = []
+try:
+    a_graph = generate_a_edges(sys.argv[1])
+    b_graph = generate_b_edges(sys.argv[2])
+    ids, cluster_number = generate_identification(sys.argv[3])
+    mixture_a = seperate_mixtures(a_graph)
+    mixture_b = seperate_mixtures(b_graph)
+    clusters = link_edges(mixture_a,mixture_b)
+except:
+    clusters = load_clusters()
 
 
 # for n in a_graph.nodes():
@@ -138,15 +172,6 @@ ids, cluster_number = generate_identification(sys.argv[3])
 #         s += 1
 
 
-
-mixture_a = seperate_mixtures(a_graph)
-mixture_b = seperate_mixtures(b_graph)
-
-for a in mixture_a:
-    for n in mixture_a[a].nodes():
-        try:
-            print(cluster_number[n])
-        except:
-            pass
-    print("----")
+c = clusters[0]
+print(c.nodes())
 # print(ids)
